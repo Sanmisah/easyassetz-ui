ba7f68a4e4394950c4826da254e9dffb264d40af
yashchaud <yashchaudhari908@gmail.com>
2024-06-07 18:24:27 +0530

    asd

--- src/components/Beneficiarydetails/Benificiarydetails.js
+++ src/components/Beneficiarydetails/Benificiarydetails.js
@@ -60,8 +60,8 @@ const Benificiarydetails = () => {
       {benificiaryData.map((data) => (
         <div className="flex border border-input p-4 justify-between pl-2 pr-2 items-center rounded-md drop-shadow-md">
           <div className="flex flex-col items-center ml-8">
-            <h1 className="font-bold">{data.fullLegalName}</h1>
-            <p className="text-sm">{data.relationship}</p>
+            <h1 className="font-bold">{data?.fullLegalName}</h1>
+            <p className="text-sm">{data?.relationship}</p>
           </div>
           <div className="flex items-center mr-8">
             <DropdownMenu>
