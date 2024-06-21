4755ce58ccacd3694b006e9b0b0e7dea3d7d1194
yashchaud <yashchaudhari908@gmail.com>
2024-06-21 11:24:47 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://demo12.sanmishatech.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
