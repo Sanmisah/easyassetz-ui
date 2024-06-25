e66db843b16cb4160e07592debca13427409ee90
yashchaud <yashchaudhari908@gmail.com>
2024-06-25 15:33:24 +0530

    asd

--- src/components/Insurance/Healthinsurance/HealthForm.js
+++ src/components/Insurance/Healthinsurance/HealthForm.js
@@ -167,7 +167,7 @@ const HealthForm = () => {
       setnomineeerror(true);
       return;
     }
-    const date = new Date(data.dob);
+    const date = new Date(data.maturityDate);
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
     const year = date.getFullYear();
