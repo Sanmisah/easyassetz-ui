79f92d2c0d53a9b35483dfd1bfb5aaad9cb64ab2
vipulkoli2000 <vipulkoli2323@gmail.com>
2024-08-13 12:13:51 +0530

    vv

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,6 +8,28 @@ const Dashboard = () => {
         This is the dashboard page where you can view your profile and
         transactions.
       </p>
+      <div className="flex space-x-4">
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+        <div className="cursor-pointer flex items-center gap-8 bg-gray-100 p-4 rounded-lg w-64 h-52">
+          comming soon!!
+        </div>
+      </div>
     </div>
   );
 };
